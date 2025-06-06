eb796a6197493da206fe328fbf40d08f44e0c0d5
danitorelli <dani.ctorelli@edu.unifil.br>
2025-06-06 15:41:32 -0300

    subindo pra railway

--- cardapio_config/settings.py
+++ cardapio_config/settings.py
@@ -91,8 +91,9 @@ WSGI_APPLICATION = 'cardapio_config.wsgi.application'
 #    }
 #}
 DATABASES = {
-    'default': dj_database_url.config(conn_max_age=600)
+    "default": dj_database_url.config(default=os.environ.get("DATABASE_URL"))
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
